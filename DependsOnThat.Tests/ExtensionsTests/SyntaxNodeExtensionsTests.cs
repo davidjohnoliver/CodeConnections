02b0657214f8aca15c9a6d9bbd4a655615e40b78
David Oliver <david.john.oliver@gmail.com>
2020-08-16 14:48:05 -0400

    Add GetAllReferencedSymbols()

---
+++ DependsOnThat.Tests/ExtensionsTests/SyntaxNodeExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿#nullable disable
+
+using DependsOnThat.Extensions;
+using DependsOnThat.Tests.Utilities;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependsOnThat.Tests.ExtensionsTests
+{
+	[TestFixture]
+	public class SyntaxNodeExtensionsTests
+	{
+		private Workspace GetSubjectSolution()
+		{
+			var location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			const string subjectPath = @"../../SubjectSolution";
+			return WorkspaceUtils.GetWorkspace(Path.Combine(location, subjectPath));
+		}
+
+		[Test]
+		public async Task When_All_Symbols()
+		{
+			using (var workspace = GetSubjectSolution())
+			{
+				var project = workspace.CurrentSolution.Projects.Single(p => p.Name == "SubjectSolution");
+				var compilation = await project.GetCompilationAsync();
+				var someClassTree = compilation.SyntaxTrees.Single(t => Path.GetFileName(t.FilePath) == "SomeClass.cs");
+				var rootNode = await someClassTree.GetRootAsync();
+				var symbols = rootNode.GetAllReferencedTypeSymbols(compilation.GetSemanticModel(someClassTree), includeExternalMetadata: false).ToArray();
+
+				Assert.IsTrue(symbols.Any(s => s.Name == "SomeOtherClass"));
+				Assert.IsTrue(symbols.Any(s => s.Name == "SomeEnumeratedClass"));
+				Assert.IsTrue(symbols.Any(s => s.Name == "SomeClassCore"));
+			}
+		}
+	}
+}
---
+++ DependsOnThat/Extensions/SyntaxNodeExtensions.cs
@@ -0,0 +1,39 @@
+﻿#nullable enable
+
+using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependsOnThat.Extensions
+{
+	public static class SyntaxNodeExtensions
+	{
+		/// <summary>
+		/// Retrieves all symbols referenced in the syntax subtree rooted on <paramref name="syntaxNode"/>.
+		/// </summary>
+		/// <param name="includeExternalMetadata">
+		/// If true, externally-defined symbols from metadata are included. If false, only locally-defined 
+		/// symbols in the same compilation are returned.
+		/// </param>
+		public static IEnumerable<ISymbol> GetAllReferencedSymbols(this SyntaxNode syntaxNode, SemanticModel model, bool includeExternalMetadata = true)
+			=> syntaxNode.DescendantNodesAndSelf()
+				.Select(n => model.GetSymbolInfo(n).Symbol)
+				.Trim()
+				.Where(s => includeExternalMetadata ? true : !s.DeclaringSyntaxReferences.IsEmpty)
+				.Distinct();
+
+		/// <summary>
+		/// Retrieves all <see cref="ITypeSymbol"/> symbols referenced in the syntax subtree rooted on <paramref name="syntaxNode"/>.
+		/// </summary>
+		/// <param name="includeExternalMetadata">
+		/// If true, externally-defined symbols from metadata are included. If false, only locally-defined 
+		/// symbols in the same compilation are returned.
+		/// </param>
+		public static IEnumerable<ITypeSymbol> GetAllReferencedTypeSymbols(this SyntaxNode syntaxNode, SemanticModel model, bool includeExternalMetadata = true)
+			=> GetAllReferencedSymbols(syntaxNode, model, includeExternalMetadata).OfType<ITypeSymbol>();
+
+	}
+}
