7ddc88ac16f230096111ef9343d470edcc350b6e
David Oliver <david.john.oliver@gmail.com>
2020-08-21 21:12:28 -0400

    Restore command initialization

--- DependsOnThat/VSIX/DependsOnThatPackage.cs
+++ DependsOnThat/VSIX/DependsOnThatPackage.cs
@@ -50,6 +50,8 @@ namespace DependsOnThat.VSIX
 			// When initialized asynchronously, the current thread may be a background thread at this point.
 			// Do any initialization that requires the UI thread after switching to the UI thread.
 			await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+			await DependencyGraphToolWindowCommand.InitializeAsync(this);
 		}
 
 		#endregion
