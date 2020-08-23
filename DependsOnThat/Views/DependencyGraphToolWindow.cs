e3b2e5b5ef3004676a9979240a49e6ce48d77dfa
David Oliver <david.john.oliver@gmail.com>
2020-08-22 20:26:36 -0400

    Adjust window caption and menu text

--- DependsOnThat/Views/DependencyGraphToolWindow.cs
+++ DependsOnThat/Views/DependencyGraphToolWindow.cs
@@ -25,7 +25,7 @@ namespace DependsOnThat.Views
 		/// </summary>
 		public DependencyGraphToolWindow() : base(null)
 		{
-			this.Caption = "DependencyGraphToolWindow";
+			this.Caption = "DependsOn Graph";
 
 			// This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
 			// we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
