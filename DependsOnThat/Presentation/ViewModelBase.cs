fc389f4e3395b20634c51537cdd9029de1b1690b
David Oliver <david.john.oliver@gmail.com>
2020-08-24 05:03:57 -0400

    Add ViewModelBase

---
+++ DependsOnThat/Presentation/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependsOnThat.Presentation
+{
+	public abstract class ViewModelBase : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnValueSet<T>(ref T currentValue, T newValue, [CallerMemberName] string name = null)
+		{
+			if (!Equals(currentValue, newValue))
+			{
+				currentValue = newValue;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+			}
+		}
+	}
+}
