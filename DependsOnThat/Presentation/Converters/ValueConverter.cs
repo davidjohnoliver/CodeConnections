be7274f8c7089645d685768ef61743909ac11ab4
David Oliver <david.john.oliver@gmail.com>
2020-08-27 20:16:59 -0400

    Add generic ValueConverter

---
+++ DependsOnThat/Presentation/Converters/ValueConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DependsOnThat.Presentation.Converters
+{
+	/// <summary>
+	/// A type-checked <see cref="IValueConverter"/>
+	/// </summary>
+	/// <typeparam name="TValue">The type of the value expected from the binding.</typeparam>
+	/// <typeparam name="TTarget">The type being converted to.</typeparam>
+	public abstract class ValueConverter<TValue, TTarget> : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value switch
+		{
+			TValue tValue => ConvertInner(tValue, parameter, culture),
+			null => ConvertNull(parameter, culture),
+			_ => ConvertIncorrectType(parameter, culture)
+		};
+
+		protected abstract TTarget ConvertInner(TValue value, object parameter, CultureInfo culture);
+		protected virtual TTarget ConvertNull(object parameter, CultureInfo culture) => ConvertInner(default(TValue), parameter, culture);
+		protected virtual object ConvertIncorrectType(object parameter, CultureInfo culture) => null;
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => value switch
+		{
+			TTarget tValue => ConvertBackInner(tValue, parameter, culture),
+			null => ConvertBackNull(parameter, culture),
+			_ => ConvertBackIncorrectType(parameter, culture)
+		};
+
+		protected virtual TValue ConvertBackInner(TTarget value, object parameter, CultureInfo culture) => throw new NotSupportedException();
+		protected virtual TValue ConvertBackNull(object parameter, CultureInfo culture) => ConvertBackInner(default(TTarget), parameter, culture);
+		protected virtual object ConvertBackIncorrectType(object parameter, CultureInfo culture) => throw new NotSupportedException();
+	}
+}
