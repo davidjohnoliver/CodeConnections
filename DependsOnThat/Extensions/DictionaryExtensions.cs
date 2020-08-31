9adcf0000c4aa9e660e5db9c855c4dedbd5f2e49
David Oliver <david.john.oliver@gmail.com>
2020-08-31 08:19:00 -0400

    Implement selected vertex

---
+++ DependsOnThat/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft;
+
+namespace DependsOnThat.Extensions
+{
+	public static class DictionaryExtensions
+	{
+		[return: MaybeNull]
+		public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+		{
+			if (dictionary.TryGetValue(key, out var value))
+			{
+				return value;
+			}
+
+			return default;
+		}
+	}
+}
--- DependsOnThat/Views/DependencyGraphLayout.cs
+++ DependsOnThat/Views/DependencyGraphLayout.cs
@@ -7,8 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using DependsOnThat.Extensions;
 using DependsOnThat.Presentation;
 using GraphSharp.Algorithms.OverlapRemoval;
+using GraphSharp.AttachedBehaviours;
 using GraphSharp.Controls;
 using QuickGraph;
 
@@ -25,6 +27,8 @@ namespace DependsOnThat.Views
 			AsyncCompute = true;
 
 			LayoutAlgorithmType = "LinLog";
+
+			HighlightAlgorithmType = "Simple";
 		}
 
 		public TimeSpan? RenderTime
@@ -72,6 +76,65 @@ namespace DependsOnThat.Views
 			{
 				RenderTime = _stopwatch.Elapsed;
 			}
+		}
+
+		public VertexControl? SelectedVertexControl
+		{
+			get { return (VertexControl?)GetValue(SelectedVertexControlProperty); }
+			set { SetValue(SelectedVertexControlProperty, value); }
+		}
+
+		public static readonly DependencyProperty SelectedVertexControlProperty =
+			DependencyProperty.Register("SelectedVertexControl", typeof(VertexControl), typeof(DependencyGraphLayout), new PropertyMetadata(null,
+				(o, e) => ((DependencyGraphLayout)o).OnSelectedVertexControlChanged((VertexControl)e.OldValue, (VertexControl)e.NewValue)));
+
+		private void OnSelectedVertexControlChanged(VertexControl? oldValue, VertexControl? newValue)
+		{
+			if (oldValue != null)
+			{
+				GraphElementBehaviour.SetHighlightTrigger(oldValue, false);
+			}
+			if (newValue != null)
+			{
+				GraphElementBehaviour.SetHighlightTrigger(newValue, true);
+			}
+			SelectedVertex = newValue?.Vertex as DisplayNode;
+		}
+
+		public DisplayNode? SelectedVertex
+		{
+			get { return (DisplayNode)GetValue(SelectedVertexProperty); }
+			set { SetValue(SelectedVertexProperty, value); }
+		}
+
+		public static readonly DependencyProperty SelectedVertexProperty =
+			DependencyProperty.Register("SelectedVertex", typeof(DisplayNode), typeof(DependencyGraphLayout), new PropertyMetadata(null,
+				(o, e) => ((DependencyGraphLayout)o).OnSelectedVertexChanged((DisplayNode)e.OldValue, (DisplayNode)e.NewValue)));
+
+		private void OnSelectedVertexChanged(DisplayNode? oldValue, DisplayNode? newValue)
+		{
+			SelectedVertexControl = newValue != null ? VertexControls.GetOrDefault(newValue) : null;
+		}
+
+		protected override void CreateVertexControl(DisplayNode vertex)
+		{
+			base.CreateVertexControl(vertex);
+			var newControl = VertexControls[vertex];
+
+			GraphElementBehaviour.SetHighlightTrigger(newControl, false); // TODO: this is lazy, should disable it through the style
+			var isDrag = DragBehaviour.GetIsDragEnabled(newControl);
+			DragBehaviour.SetIsDragEnabled(newControl, false); //
+			newControl.MouseLeftButtonUp += OnVertexControlMouseLeftButtonUp;
+			DragBehaviour.SetIsDragEnabled(newControl, isDrag);
+		}
+
+		private void OnVertexControlMouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e) => SelectedVertexControl = sender as VertexControl;
+
+		protected override void RemoveVertexControl(DisplayNode vertex)
+		{
+			var oldControl = VertexControls[vertex];
+			base.RemoveVertexControl(vertex);
+			oldControl.MouseLeftButtonUp -= OnVertexControlMouseLeftButtonUp;
 		}
 	}
 }
