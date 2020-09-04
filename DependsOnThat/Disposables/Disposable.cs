956b00392dc650640a159efc3b30df067571ebf7
David Oliver <david.john.oliver@gmail.com>
2020-09-04 05:47:41 -0400

    Add Disposable.Create()

---
+++ DependsOnThat/Disposables/Disposable.cs
@@ -0,0 +1,31 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependsOnThat.Disposables
+{
+	public static class Disposable
+	{
+		/// <summary>
+		/// Creates a lightweight disposable.
+		/// </summary>
+		/// <param name="action">The action to be executed on disposal.</param>
+		public static IDisposable Create(Action action) => new DisposableAction(action ?? throw new ArgumentNullException(nameof(action)));
+
+		private class DisposableAction : IDisposable
+		{
+			private readonly Action _action;
+
+			public DisposableAction(Action action)
+			{
+				_action = action;
+			}
+
+			public void Dispose() => _action.Invoke();
+		}
+	}
+}
