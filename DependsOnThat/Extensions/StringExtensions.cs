da1fc3f886c4dfa5e007619a3f682098c9768910
David Oliver <david.john.oliver@gmail.com>
2020-09-06 13:58:29 -0400

    Add StringExtensions

---
+++ DependsOnThat/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependsOnThat.Extensions
+{
+	public static class StringExtensions
+	{
+		public static bool IsNullOrEmpty([NotNullWhen(false)] this string? str) => string.IsNullOrEmpty(str);
+
+		public static bool IsNullOrWhiteSpace([NotNullWhen(false)] this string? str) => string.IsNullOrWhiteSpace(str);
+	}
+}
