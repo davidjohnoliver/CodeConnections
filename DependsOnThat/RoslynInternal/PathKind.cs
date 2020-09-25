9049c48306733604adf250d93c332ea1abd5e661
David Oliver <david.john.oliver@gmail.com>
2020-09-24 20:48:31 -0400

    Import internal Roslyn files
    The aim here is to make use of the IsGeneratedCode() utility method.

---
+++ DependsOnThat/RoslynInternal/PathKind.cs
@@ -0,0 +1,44 @@
+﻿// https://github.com/dotnet/roslyn/blob/version-3.2.0/src/Compilers/Core/Portable/FileSystem/PathKind.cs
+
+// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+namespace Roslyn.Utilities
+{
+	internal enum PathKind
+	{
+		/// <summary>
+		/// Null or empty.
+		/// </summary>
+		Empty,
+
+		/// <summary>
+		/// "file"
+		/// </summary>
+		Relative,
+
+		/// <summary>
+		/// ".\file"
+		/// </summary>
+		RelativeToCurrentDirectory,
+
+		/// <summary>
+		/// "..\file"
+		/// </summary>
+		RelativeToCurrentParent,
+
+		/// <summary>
+		/// "\dir\file"
+		/// </summary>
+		RelativeToCurrentRoot,
+
+		/// <summary>
+		/// "C:dir\file"
+		/// </summary>
+		RelativeToDriveDirectory,
+
+		/// <summary>
+		/// "C:\file" or "\\machine" (UNC).
+		/// </summary>
+		Absolute,
+	}
+}
