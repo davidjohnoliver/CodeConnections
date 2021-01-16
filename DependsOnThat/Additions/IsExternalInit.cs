59fad582f8ebe23b1b4218f313ac2c6fadcf3e9c
David Oliver <david.john.oliver@gmail.com>
2021-01-16 11:47:27 -0500

    Enable C# 9 init and records

---
+++ DependsOnThat/Additions/IsExternalInit.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Runtime.CompilerServices
+{
+	internal static class IsExternalInit { } // https://stackoverflow.com/questions/62648189/testing-c-sharp-9-0-in-vs2019-cs0518-isexternalinit-is-not-defined-or-imported
+}
