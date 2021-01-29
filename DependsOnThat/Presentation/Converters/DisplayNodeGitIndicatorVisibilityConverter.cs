32a685f80cdeb3c3c935680f316028dc3de388fa
David Oliver <david.john.oliver@gmail.com>
2021-01-29 08:49:25 -0500

    Show badge on Git-modified files

---
+++ DependsOnThat/Presentation/Converters/DisplayNodeGitIndicatorVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using DependsOnThat.Git;
+using DependsOnThat.Graph.Display;
+
+namespace DependsOnThat.Presentation.Converters
+{
+	public class DisplayNodeGitIndicatorVisibilityConverter : MultiValueConverter<GitStatus, DisplayMode, Visibility>
+	{
+		protected override Visibility ConvertInner(GitStatus value1, DisplayMode value2, object parameter, CultureInfo culture)
+		{
+			if (value1 != GitStatus.Unchanged && value2 == DisplayMode.Git)
+			{
+				return Visibility.Visible;
+			}
+
+			return Visibility.Collapsed;
+		}
+
+		protected override Visibility ConvertUnexpected(object parameter, CultureInfo culture) => Visibility.Collapsed;
+	}
+}
---
+++ DependsOnThat/Presentation/Converters/GitStatusTooltipConverter.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DependsOnThat.Git;
+
+namespace DependsOnThat.Presentation.Converters
+{
+	public class GitStatusTooltipConverter : ValueConverter<GitStatus, string?>
+	{
+		protected override string? ConvertInner(GitStatus value, object parameter, CultureInfo culture) => value switch
+		{
+			GitStatus.Modified => "Modified file",
+			GitStatus.New => "New file",
+			_ => null
+		};
+
+		protected override string? ConvertNull(object parameter, CultureInfo culture) => null;
+	}
+}
