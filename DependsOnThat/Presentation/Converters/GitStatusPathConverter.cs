827aa76341686970c3e8003611fcdcdaa118f020
David Oliver <david.john.oliver@gmail.com>
2021-02-01 20:03:24 -0500

    Use asterisk and plus signs in Git status badges

---
+++ DependsOnThat/Presentation/Converters/GitStatusPathConverter.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using DependsOnThat.Git;
+using static DependsOnThat.Git.GitStatus;
+
+namespace DependsOnThat.Presentation.Converters
+{
+	public class GitStatusPathConverter : ValueConverter<GitStatus, Geometry?>
+	{
+		public Geometry? ModifiedFileData { get; set; }
+		public Geometry? NewFileData { get; set; }
+
+		protected override Geometry? ConvertInner(GitStatus value, object parameter, CultureInfo culture) => value switch
+		{
+			Modified => ModifiedFileData,
+			New => NewFileData,
+			_ => null
+		};
+
+		protected override Geometry? ConvertNull(object parameter, CultureInfo culture) => null;
+	}
+}
--- acknowledgements.md
+++ acknowledgements.md
@@ -1,3 +1,5 @@
 DependsOnThat incorporates code from the following sources:
 
 [Uno.Core](https://github.com/unoplatform/uno.core), used under the Apache 2.0 license
+
+Font Awesome glyphs (https://commons.wikimedia.org/wiki/File:Asterisk_font_awesome.svg and https://commons.wikimedia.org/wiki/File:Plus_font_awesome.svg), used under the Creative Commons BY-SA 3.0 license
