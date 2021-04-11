6d465a581d952a6e44ccda31edfada5e948ad020
David Oliver <david.john.oliver@gmail.com>
2021-04-11 16:25:06 -0400

    Delay creation of output pane
    Create output pane when needed rather than eagerly, for consistency with other pane types.

--- CodeConnections/Services/OutputWindowOutputService.cs
+++ CodeConnections/Services/OutputWindowOutputService.cs
@@ -12,16 +12,37 @@ namespace CodeConnections.Services
 {
 	internal class OutputWindowOutputService : IOutputService
 	{
-		IVsOutputWindowPane _outputPane;
+		IVsOutputWindowPane? _outputPane;
+
+		/// <summary>
+		/// Have we already created (or tried and failed to create) the output pane?
+		/// </summary>
+		private bool _hasCreatedOutputPane;
+		private readonly string _guidString;
+		private readonly IVsOutputWindow _outputWindow;
+		private readonly string _outputPaneName;
 
 		public OutputLevel CurrentOutputLevel { get; set; }
 
 		public OutputWindowOutputService(string guidString, IVsOutputWindow outputWindow, string outputPaneName)
 		{
+			_guidString = guidString;
+			_outputWindow = outputWindow;
+			_outputPaneName = outputPaneName;
+		}
+
+		private void TryCreateOutputPane()
+		{
+			if (_hasCreatedOutputPane)
+			{
+				return;
+			}
+			_hasCreatedOutputPane = true;
+
 			Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
-			var guid = new Guid(guidString);
-			outputWindow.CreatePane(guid, outputPaneName, 1, 1);
-			outputWindow.GetPane(guid, out _outputPane);
+			var guid = new Guid(_guidString);
+			_outputWindow.CreatePane(guid, _outputPaneName, 1, 1);
+			_outputWindow.GetPane(guid, out _outputPane);
 			if (_outputPane == null)
 			{
 				throw new InvalidOperationException("Failed to create output pane");
@@ -31,13 +52,16 @@ namespace CodeConnections.Services
 		public void WriteLine(string output)
 		{
 			Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
-			_outputPane.OutputString($"{output}{Environment.NewLine}");
+			TryCreateOutputPane();
+
+			_outputPane?.OutputString($"{output}{Environment.NewLine}");
 		}
 
 		public void FocusOutput()
 		{
 			Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
-			_outputPane.Activate();
+			TryCreateOutputPane();
+			_outputPane?.Activate();
 		}
 
 		public bool IsEnabled(OutputLevel outputLevel) => outputLevel <= CurrentOutputLevel;
