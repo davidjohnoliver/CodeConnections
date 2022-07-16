6147e7f64a997fbe451eb191dbacf309449a665b
David Oliver <david.john.oliver@gmail.com>
2022-07-16 06:36:40 -0400

    Add WeakReferenceExtensions

---
+++ CodeConnections.Shared/Extensions/WeakReferenceExtensions.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeConnections.Extensions
+{
+	public static class WeakReferenceExtensions
+	{
+		public static T? TargetOrDefault<T>(this WeakReference<T> weakRef) where T : class
+		{
+			if (weakRef.TryGetTarget(out T target))
+			{
+				return target;
+			}
+
+			return default;
+		}
+	}
+}
