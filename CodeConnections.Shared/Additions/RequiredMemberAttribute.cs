5e53f9dc31a1161e574a8635bddbd2b665d3f6c3
David Oliver <david.john.oliver@gmail.com>
2023-01-07 20:58:54 -0500

    Add attributes required for required
    See what I did there

---
+++ CodeConnections.Shared/Additions/RequiredMemberAttribute.cs
@@ -0,0 +1,17 @@
+﻿// https://github.com/dotnet/runtime/blob/f08d13f7f912dd3d89508b3ccd48f47c85c56ddd/src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/RequiredMemberAttribute.cs
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Runtime.CompilerServices
+{
+	/// <summary>Specifies that a type has required members or that a member is required.</summary>
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+#if SYSTEM_PRIVATE_CORELIB
+    public
+#else
+	internal
+#endif
+		sealed class RequiredMemberAttribute : Attribute
+	{ }
+}
---
+++ CodeConnections.Shared/Additions/SetsRequiredMembersAttribute.cs
@@ -0,0 +1,20 @@
+﻿// https://github.com/dotnet/runtime/blob/f08d13f7f912dd3d89508b3ccd48f47c85c56ddd/src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/SetsRequiredMembersAttribute.cs
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Diagnostics.CodeAnalysis
+{
+	/// <summary>
+	/// Specifies that this constructor sets all required members for the current type, and callers
+	/// do not need to set any required members themselves.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+#if SYSTEM_PRIVATE_CORELIB
+    public
+#else
+	internal
+#endif
+		sealed class SetsRequiredMembersAttribute : Attribute
+	{ }
+}
