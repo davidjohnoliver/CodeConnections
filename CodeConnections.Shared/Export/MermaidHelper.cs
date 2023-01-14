d8bb0ca830242fe7837c5bf4b73a0a6af88d2efd
David Oliver <david.john.oliver@gmail.com>
2023-01-14 13:51:45 -0500

    Add Mermaid diagram generation

---
+++ CodeConnections.Shared/Export/MermaidHelper.cs
@@ -0,0 +1,53 @@
+﻿#nullable enable
+
+using CodeConnections.Graph;
+using CodeConnections.Graph.Display;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using _IDisplayGraph = QuickGraph.IBidirectionalGraph<CodeConnections.Graph.Display.DisplayNode, CodeConnections.Graph.Display.DisplayEdge>;
+
+namespace CodeConnections.Export
+{
+	public static class MermaidHelper
+	{
+		/// <summary>
+		/// Generate a Mermaid diagram from <paramref name="graph"/>. See https://mermaid.js.org/syntax/flowchart.html
+		/// </summary>
+		public static string GenerateMermaidGraph(_IDisplayGraph graph)
+		{
+			var mermaidIds = new Dictionary<DisplayNode, string>();
+			int idCounter = -1;
+
+			var sb = new StringBuilder();
+
+			sb.AppendLine("graph BT");
+
+			foreach (var edge in graph.Edges)
+			{
+				var mermaidEdge = $"    {GetVertexEntry(edge.Source)} --> {GetVertexEntry(edge.Target)}";
+				sb.AppendLine(mermaidEdge);
+			}
+
+			// TODO-export: append vertices that don't have any edges
+
+			return sb.ToString();
+
+			string GetVertexEntry(DisplayNode vertex)
+			{
+				if (mermaidIds.TryGetValue(vertex, out var existing))
+				{
+					return existing;
+				}
+
+				idCounter++;
+
+				var mermaidId = $"CC{idCounter}";
+				mermaidIds[vertex] = mermaidId;
+
+				// TODO-export: sanitize eg angle brackets
+				return $"{mermaidId}({vertex.DisplayString})";
+			}
+		}
+	}
+}
